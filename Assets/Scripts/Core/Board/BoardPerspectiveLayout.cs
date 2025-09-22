a8937af4b466bf69069a10d8f37e91b523fb3383
kjleblanc <kenjleblanc@Live.com>
2025-09-22 00:37:00 -0400

    Update BoardPerspectiveLayout.cs

--- Assets/Scripts/Core/Board/BoardPerspectiveLayout.cs
+++ Assets/Scripts/Core/Board/BoardPerspectiveLayout.cs
@@ -102,27 +102,72 @@ namespace MergeDungeon.Core
                 }
             }
 
-            float totalWidth = _controller.width * cellSize.x + Mathf.Max(0, _controller.width - 1) * spacing.x + padding.left + padding.right;
             float totalHeight = _controller.height * cellSize.y + Mathf.Max(0, _controller.height - 1) * spacing.y + padding.top + padding.bottom;
 
-            float startX = -totalWidth * 0.5f + padding.left + cellSize.x * 0.5f;
             float startY = totalHeight * 0.5f - padding.top - cellSize.y * 0.5f;
 
-            float stepX = cellSize.x + spacing.x;
             float stepY = cellSize.y + spacing.y;
+            float tiltRad = tiltAngle * Mathf.Deg2Rad;
             float columnCenter = _controller.width > 1 ? (_controller.width - 1) * 0.5f : 0f;
+
+            int rowCount = _controller.height;
+            if (rowCount <= 0) return;
+
+            int columnCount = _controller.width;
+            int columnsMinusOne = Mathf.Max(0, columnCount - 1);
+            var rowScales = new float[rowCount];
+            var rowDesignerYOffset = new float[rowCount];
+            var rowSkewPerColumn = new float[rowCount];
+            var rowDesignerZOffset = new float[rowCount];
+            var rowStartX = new float[rowCount];
+            var rowStepX = new float[rowCount];
+
+            for (int y = 0; y < rowCount; y++)
+            {
+                float rowNormalized = rowCount <= 1 ? 0f : (float)y / (rowCount - 1);
+                float scale = perRowScale != null ? perRowScale.Evaluate(rowNormalized) : 1f;
+                rowScales[y] = scale;
+                rowDesignerYOffset[y] = rowDepthOffset * rowNormalized;
+                rowSkewPerColumn[y] = horizontalSkew * rowNormalized;
+                rowDesignerZOffset[y] = rowZOffset * rowNormalized;
+
+                float scaledCellWidth = cellSize.x * scale;
+                float rowTotalWidth = scaledCellWidth * columnCount + columnsMinusOne * spacing.x;
+                float paddedRowWidth = rowTotalWidth + padding.left + padding.right;
+                rowStepX[y] = scaledCellWidth + spacing.x;
+                rowStartX[y] = -paddedRowWidth * 0.5f + padding.left + scaledCellWidth * 0.5f;
+            }
+
+            var cumulativeCompression = new float[rowCount];
+            var cumulativeDepth = new float[rowCount];
+            for (int y = 1; y < rowCount; y++)
+            {
+                float lowerScale = rowScales[y - 1];
+                float currentScale = rowScales[y];
+                float halfHeightLower = cellSize.y * lowerScale * 0.5f;
+                float halfHeightCurrent = cellSize.y * currentScale * 0.5f;
+                float spacingBeforeTilt = halfHeightLower + spacing.y + halfHeightCurrent;
+                float desiredYSpacing = spacingBeforeTilt * Mathf.Cos(tiltRad);
+                float desiredDepthSpacing = spacingBeforeTilt * Mathf.Sin(tiltRad);
+
+                cumulativeCompression[y] = cumulativeCompression[y - 1] + (stepY - desiredYSpacing);
+                cumulativeDepth[y] = cumulativeDepth[y - 1] + desiredDepthSpacing;
+            }
 
             var rotation = Quaternion.Euler(tiltAngle, 0f, 0f);
 
-            for (int y = 0; y < _controller.height; y++)
+            for (int y = 0; y < rowCount; y++)
             {
-                float rowNormalized = _controller.height <= 1 ? 0f : (float)y / (_controller.height - 1);
-                float scale = perRowScale != null ? perRowScale.Evaluate(rowNormalized) : 1f;
-                float offsetY = rowDepthOffset * rowNormalized;
-                float skewPerColumn = horizontalSkew * rowNormalized;
-                float offsetZ = rowZOffset * rowNormalized;
+                float scale = rowScales[y];
+                float offsetY = rowDesignerYOffset[y];
+                float skewPerColumn = rowSkewPerColumn[y];
+                float offsetZ = rowDesignerZOffset[y];
+                float compressionOffset = cumulativeCompression[y];
+                float depthOffset = cumulativeDepth[y];
+                float baseRowStartX = rowStartX[y];
+                float rowStep = rowStepX[y];
 
-                for (int x = 0; x < _controller.width; x++)
+                for (int x = 0; x < columnCount; x++)
                 {
                     var cell = _controller.GetCell(x, y);
                     if (cell == null) continue;
@@ -137,14 +182,16 @@ namespace MergeDungeon.Core
                     rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, cellSize.x);
                     rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, cellSize.y);
 
-                    int rowFromTop = _controller.height - 1 - y;
-                    float baseX = startX + x * stepX;
+                    int rowFromTop = rowCount - 1 - y;
+                    float baseX = baseRowStartX + x * rowStep;
                     float baseY = startY - rowFromTop * stepY;
+                    float adjustedY = baseY - compressionOffset + offsetY;
+                    float adjustedZ = depthOffset + offsetZ;
 
                     float centerDelta = x - columnCenter;
                     float skewOffset = skewPerColumn * centerDelta;
 
-                    rt.anchoredPosition3D = new Vector3(baseX + skewOffset, baseY + offsetY, offsetZ);
+                    rt.anchoredPosition3D = new Vector3(baseX + skewOffset, adjustedY, adjustedZ);
                     rt.localScale = new Vector3(scale, scale, 1f);
                     rt.localRotation = rotation;
                 }
@@ -157,3 +204,11 @@ namespace MergeDungeon.Core
         }
     }
 }
+
+
+
+
+
+
+
+
